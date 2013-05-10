87e648e1777916fb89b2bbb4af2e22dbe0cb2b25
tshirtman <gabriel.pettier@gmail.com>
2013-05-10 16:11:27 +0200

    Initial commit
    Menu screen with slide transition to add and Backlog screens (just back atm)

---
+++ main.py
@@ -0,0 +1,22 @@
+from kivy.app import App
+from kivy.uix.screenmanager import Screen
+
+
+class NdfApp(App):
+    pass
+
+
+class MenuScreen(Screen):
+    pass
+
+
+class AddScreen(Screen):
+    pass
+
+
+class BacklogScreen(Screen):
+    pass
+
+
+if __name__ == '__main__':
+    NdfApp().run()
