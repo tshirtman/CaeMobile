6932ca2c6a2971bb29dc3446150e558f13f1b1cb
tshirtman <gabriel.pettier@gmail.com>
2013-06-04 15:17:56 +0200

    work on sync popup

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 from kivy.app import App
 from kivy.uix.screenmanager import Screen
 from kivy.uix.listview import ListItemButton
+from kivy.uix.popup import Popup
 from kivy.adapters.simplelistadapter import SimpleListAdapter
-from kivy.properties import ObjectProperty, ListProperty, StringProperty, AliasProperty
+from kivy.properties import (
+        ObjectProperty, ListProperty, StringProperty, AliasProperty,
+        BooleanProperty, NumericProperty)
 
+from kivy.network.urlrequest import UrlRequest
 from ConfigParser import SafeConfigParser
 
 DEFAULTSETTINGSFILE= '.default_config.ini'
@@ -13,10 +17,17 @@ SETTINGSFILE = 'config.ini'
 __version__ = '0.01'
 
 
+class SyncPopup(Popup):
+    progress = NumericProperty(0)
+    done = BooleanProperty(False)
+    errors = ListProperty([])
+
+
 class NdfApp(App):
     datalist_adapter = ObjectProperty(None)
     datalist = ListProperty([])
     settings = ObjectProperty()
+    requests = ListProperty([])
 
     def __init__(self, **kwargs):
         super (NdfApp, self).__init__(**kwargs)
@@ -38,6 +49,34 @@ class NdfApp(App):
 
         print self.datalist_adapter
         return super(NdfApp, self).build()
+
+    def send(self, note):
+        body = {
+        }
+
+        self.requests.append(UrlRequest(
+            self.settings.get('settings', 'server'),
+            on_success=self.send_success,
+            on_failure=self.send_failure,
+            req_body=body
+            )
+        )
+
+    def send_success(self, *args):
+        self.settings.remove_option('tosync', n[0])
+        self.popup.progress += 1
+
+    def send_failure(self, *args):
+        self.popup.progress += 1
+        self.popup.errors.append('error treating %s: %s' % (n[0], e))
+
+    def sync(self, *args):
+        notes = self.settings.items('tosync')
+        self.popup = SyncPopup()
+        self.popup.open()
+
+        for n in notes:
+            self.send(n)
 
     def on_pause(self, *args):
         with open(SETTINGSFILE, 'w') as f:
